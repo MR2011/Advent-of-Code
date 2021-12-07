2ea35b351758cd363e59d3e29663e0f65f26fff4
Michael <mr201190@gmail.com>
2021-12-07 20:32:47 +0100

    add day3

--- 2021/src/Day03.hs
+++ 2021/src/Day03.hs
@@ -1,38 +1,33 @@
-{-# LANGUAGE ViewPatterns #-}
-
 module Day03 (part1, part2) where
 
-import Data.List.Split (splitOn)
+import Data.List (transpose)
+import Helpers (invertBinary, mostCommon, stringToDec)
 import Paths_advent_of_code (getDataFileName)
 
 part1 :: IO Int
 part1 = do
-  (x, y) <- foldl move (0, 0) <$> input
-  return $ x * y
-  where
-    move :: (Int, Int) -> (String, Int) -> (Int, Int)
-    move (x, y) (cmd, n)
-      | cmd == "forward" = (x + n, y)
-      | cmd == "up" = (x, y - n)
-      | cmd == "down" = (x, y + n)
-      | otherwise = (x, y)
+  gamma <- map mostCommon . transpose <$> input
+  let epsilon = invertBinary gamma
+  return $ stringToDec gamma * stringToDec epsilon
 
 part2 :: IO Int
 part2 = do
-  (x, y, _) <- foldl move (0, 0, 0) <$> input
-  return $ x * y
-  where
-    move :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
-    move (x, y, z) (cmd, n)
-      | cmd == "forward" = (x + n, y + (z * n), z)
-      | cmd == "up" = (x, y, z - n)
-      | cmd == "down" = (x, y, z + n)
-      | otherwise = (x, y, z)
+  report <- input
+  let oxygen = rate (>=) 0 report
+  let co2 = rate (<) 0 report
+  return $ stringToDec oxygen * stringToDec co2
 
-input :: IO [(String, Int)]
-input = map parse . lines <$> (readFile =<< getDataFileName "inputs/day02.txt")
+rate :: (Int -> Int -> Bool) -> Int -> [String] -> String
+rate op i xs =
+  if length xs > 1
+    then rate op (i + 1) $ filter (\x -> x !! i == map (criteria op) (transpose xs) !! i) xs
+    else head xs
 
-parse :: String -> (String, Int)
-parse line = (cmd, value)
-  where
-    [cmd, read -> value] = splitOn " " line
+criteria :: (Int -> Int -> Bool) -> String -> Char
+criteria op s = if op (countChar s '1') (countChar s '0') then '1' else '0'
+
+countChar :: String -> Char -> Int
+countChar s c = length $ filter (== c) s
+
+input :: IO [[Char]]
+input = lines <$> (readFile =<< getDataFileName "inputs/day03.txt")
--- 2021/src/Helpers.hs
+++ 2021/src/Helpers.hs
@@ -1,16 +1,22 @@
 module Helpers
-    ( readInts
-    , readCommaSeparatedInts
-    )
+  ( readInts,
+    readCommaSeparatedInts,
+    mostCommon,
+    leastCommon,
+    invertBinary,
+    stringToDec,
+  )
 where
 
-import           Data.List.Split                ( splitOn )
-
-import           Paths_advent_of_code
+import Data.Char (digitToInt)
+import Data.List (foldl', group, maximumBy, minimumBy, sort)
+import Data.List.Split (splitOn)
+import Data.Ord (comparing)
+import Paths_advent_of_code
 
 readInts :: FilePath -> IO [Int]
 readInts filePath =
-    map read . lines <$> (readFile =<< getDataFileName filePath)
+  map read . lines <$> (readFile =<< getDataFileName filePath)
 
 -- readCommaSeparatedInts :: FilePath -> IO [Int]
 -- readCommaSeparatedInts filePath =
@@ -19,3 +25,15 @@ readInts filePath =
 readCommaSeparatedInts :: String -> [Int]
 readCommaSeparatedInts = map read . splitOn ","
 
+mostCommon :: Ord a => [a] -> a
+mostCommon = head . maximumBy (comparing length) . group . sort
+
+leastCommon :: Ord a => [a] -> a
+leastCommon = last . maximumBy (comparing length) . group . sort
+
+
+invertBinary :: String -> String
+invertBinary = map (\c -> "10" !! read [c])
+
+stringToDec :: String -> Int
+stringToDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
