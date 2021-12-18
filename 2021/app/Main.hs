614c771e995a5aa147ff01e225b4a1e51335c9e9
Michael <mr201190@gmail.com>
2021-12-18 14:42:35 +0100

    add day04

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -7,6 +7,7 @@ import           System.TimeIt                  ( timeIt )
 import           Day01
 import           Day02
 import           Day03
+import           Day04
 
 main :: IO ()
 main = do
@@ -18,6 +19,7 @@ run :: Int -> IO ()
 run  1 = runPretty 1 (Day01.part1, Day01.part2)
 run  2 = runPretty 2 (Day02.part1, Day02.part2)
 run  3 = runPretty 3 (Day03.part1, Day03.part2)
+run  4 = runPretty 4 (Day04.part1, Day04.part2)
 
 runPretty :: (Show a, Show b) => Int -> (IO a, IO b) -> IO ()
 runPretty day (part1, part2) = do
---
+++ 2021/src/Day04.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Day04
+  ( part1,
+    part2,
+  )
+where
+
+import Data.List
+  ( find,
+    transpose,
+  )
+import Data.List.Split.Internals (chunksOf)
+import Helpers
+  ( readCommaSeparatedInts,
+  )
+import Paths_advent_of_code (getDataFileName)
+
+type BingoBoard = [[(Int, Bool)]]
+
+part1 :: IO Int
+part1 = do
+  (drawings, boards) <- input
+  let (drawing, winner) = playBingo drawings boards
+  case checkSum drawing <$> winner of
+    Just score -> return score
+    Nothing -> return $ -1
+
+part2 :: IO Int
+part2 = do
+  (drawings, boards) <- input
+  let (drawing, winner) = playBingo' drawings boards
+  case checkSum drawing <$> winner of
+    Just score -> return score
+    Nothing -> return $ -1
+
+markNum :: Int -> BingoBoard -> BingoBoard
+markNum num = map (map (\(n, m) -> (n, m || n == num)))
+
+checkWinner :: BingoBoard -> Bool
+checkWinner board = any (all snd) (board ++ transpose board)
+
+firstWinner :: [BingoBoard] -> Bool
+firstWinner = any checkWinner
+
+allWinner :: [BingoBoard] -> Bool
+allWinner = all checkWinner
+
+input :: IO ([Int], [BingoBoard])
+input = do
+  l <-
+    filter (/= []) . lines <$> (readFile =<< getDataFileName "inputs/day04.txt")
+  let firstRow = readCommaSeparatedInts $ head l
+  let bingoBoards = chunksOf 5 $ map (map ((,False) . read) . words) $ tail l
+  return (firstRow, bingoBoards)
+
+markAll :: Int -> [BingoBoard] -> [BingoBoard]
+markAll num = map (markNum num)
+
+checkSum :: Int -> BingoBoard -> Int
+checkSum num board = unmarkedSum * num
+  where
+    unmarkedSum = sum [n | (n, marked) <- concat board, not marked]
+
+-- part 1
+playBingo :: [Int] -> [BingoBoard] -> (Int, Maybe BingoBoard)
+playBingo (drawing : drawings) cards = do
+  let cards' = markAll drawing cards
+  if firstWinner cards'
+    then (drawing, find checkWinner cards')
+    else playBingo drawings cards'
+
+-- part 2
+playBingo' :: [Int] -> [BingoBoard] -> (Int, Maybe BingoBoard)
+playBingo' (drawing : drawings) cards = do
+  let cards' = markAll drawing cards
+  if allWinner cards'
+    then (drawing, markNum drawing <$> find (not . checkWinner) cards)
+    else playBingo' drawings cards'
