5f3b7b44259d4a7151e477be8f21b1f17a82a3d7
Michael <mr201190@gmail.com>
2022-01-01 15:50:23 +0100

    format

--- 2021/src/Day06.hs
+++ 2021/src/Day06.hs
@@ -1,38 +1,36 @@
-
 module Day06
   ( part1,
     part2,
   )
 where
 
-import Paths_advent_of_code (getDataFileName)
 import Helpers (readCommaSeparatedInts)
-
+import Paths_advent_of_code (getDataFileName)
 
 part1 :: IO Int
-part1 = do 
+part1 = do
   l <- initCountList <$> input
-  return $ sum $ iterate step l !! 80 
+  return $ sum $ iterate step l !! 80
 
 part2 :: IO Int
-part2 = do 
+part2 = do
   l <- initCountList <$> input
-  return $ sum $ iterate step l !! 256 
+  return $ sum $ iterate step l !! 256
 
- -- Groups of numbers [0..8] 
- -- 0 spawns new fish (a moves to the end)
- -- 0 becomes 6 (a+h) 
+-- Groups of numbers [0..8]
+-- 0 spawns new fish (a moves to the end)
+-- 0 becomes 6 (a+h)
 step :: [Int] -> [Int]
-step [a,b,c,d,e,f,g,h,i] = [b,c,d,e,f,g,a+h,i,a]
+step [a, b, c, d, e, f, g, h, i] = [b, c, d, e, f, g, a + h, i, a]
 step _ = []
 
 -- Numbers: 0-8
--- Instead of managing each fish/number, group them and manage the groups 
+-- Instead of managing each fish/number, group them and manage the groups
 initCountList :: [Int] -> [Int]
-initCountList l = [count i l | i <- [0..8]]
+initCountList l = [count i l | i <- [0 .. 8]]
 
 count :: Int -> [Int] -> Int
-count v l =  length $ filter (==v) l
+count v l = length $ filter (== v) l
 
 input :: IO [Int]
 input = readCommaSeparatedInts "inputs/day06.txt"
